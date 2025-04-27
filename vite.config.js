f2399fa792fbbb84f4f9c3b931c3a0c0ade521c9
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-04-27 22:38:23 +0300

    test33

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
   css: {
-    postcss: "./postcss.config.js",
+    postcss: './postcss.config.js',
   },
   build: {
-    target: "es2015",
+    target: ['es2015'], // <<< أهم حاجة هنا عشان Safari يفهم
   },
 });
+
 
 /* 
 
