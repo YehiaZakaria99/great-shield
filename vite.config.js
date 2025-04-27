b75e143c4a31f68edd100405282ad41b0235ea54
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-04-27 22:29:58 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,42 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  build: {
+    target: "es2015",
+  },
 });
+
+/* 
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   css: {
+//     postcss: './postcss.config.js',
+//   },
+//   build: {
+//     target: 'es2015', 
+//   },
+// })
+
+
+
+
+
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  content: ["./index.html", "./src/**/
+// *.{js,ts,jsx,tsx}"],
+// });
+
+// */
